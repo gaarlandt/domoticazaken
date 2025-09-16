7d97ac98f74d94491fa8494210910ca8c1c53e19
Jurriaan <git@gaarlandt.com>
2025-09-16 15:55:31 +0200

    third try

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js/");
   eleventyConfig.addPassthroughCopy("./src/img/");
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy("./src/slider-images/");
+  eleventyConfig.addPassthroughCopy("./src/css/main.css");
   
   // Add .nojekyll file to disable Jekyll processing on GitHub Pages
   eleventyConfig.addPassthroughCopy({
@@ -12,6 +15,7 @@ module.exports = function(eleventyConfig) {
       input: "src",
       output: "_site",
       includes: "_includes"
-    }
+    },
+    pathPrefix: process.env.ELEVENTY_ENV === "prod" ? "/domoticazaken/" : "/"
   };
 };
