f1c081e91eee885be8b4534b634e9181c953cb66
Jurriaan <git@gaarlandt.com>
2025-12-12 15:34:02 +0100

    Update deploy workflow details, Eleventy config, and prune CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js/");
-  eleventyConfig.addPassthroughCopy("./src/img/");
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy("./src/slider-images/");
   eleventyConfig.addPassthroughCopy("./src/css/main.css");
-  
+
   // Add .nojekyll file to disable Jekyll processing on GitHub Pages
   eleventyConfig.addPassthroughCopy({
     "src/_nojekyll": ".nojekyll"
